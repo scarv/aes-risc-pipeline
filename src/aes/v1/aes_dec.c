f776a1c45242f93d6a1da10292b1d3547adce7be
Ben Marshall <ben.marshall@bristol.ac.uk>
2020-03-30 14:42:10 +0100

    AES 128 V1 decode block ASM
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	modified:   src/aes/share/aes_common.S
    	modified:   src/aes/v1/aes.S
    	modified:   src/aes/v1/aes_dec.c
    
     Changes not staged for commit:
    	modified:   extern/riscv-gnu-toolchain (modified content)
    	modified:   extern/riscv-isa-sim (modified content)
    	modified:   extern/scarv-cpu (modified content, untracked content)

--- src/aes/v1/aes_dec.c
+++ src/aes/v1/aes_dec.c
@@ -27,103 +27,12 @@ extern void    aes_key_schedule (
 @param [in]  rk - The expanded key schedule
 @param [in]  nr - Number of decryption rounds to perform.
 */
-void    aes_dec_block (
+extern void aes_dec_block (
     uint8_t     pt [16],
     uint8_t     ct [16],
     uint32_t  * rk,
     int         nr
-){
-    
-    int      round;
-    uint32_t n0, n1, n2, n3;
-    uint32_t t0, t1, t2, t3;
-
-    AES_LOAD_STATE(t0,t1,t2,t3,ct);
-
-    t0 ^= rk[4*nr + 0];
-    t1 ^= rk[4*nr + 1];
-    t2 ^= rk[4*nr + 2];
-    t3 ^= rk[4*nr + 3];
-
-    for(round = nr-1; round >= 1; round --) {
-
-        //
-        // Inv ShiftRows
-        
-        n0 = (t0 & 0x000000FF) | (t3 & 0x0000FF00) |
-             (t2 & 0x00FF0000) | (t1 & 0xFF000000) ;
-        
-        n1 = (t1 & 0x000000FF) | (t0 & 0x0000FF00) | 
-             (t3 & 0x00FF0000) | (t2 & 0xFF000000) ;
-        
-        n2 = (t2 & 0x000000FF) | (t1 & 0x0000FF00) |
-             (t0 & 0x00FF0000) | (t3 & 0xFF000000) ;
-
-        n3 = (t3 & 0x000000FF) | (t2 & 0x0000FF00) |
-             (t1 & 0x00FF0000) | (t0 & 0xFF000000) ;
-
-        //
-        // Inv SubBytes
-
-        t0 = _saes_v1_decs(n0);
-        t1 = _saes_v1_decs(n1);
-        t2 = _saes_v1_decs(n2);
-        t3 = _saes_v1_decs(n3);
-
-        //
-        // Add Round Key
-
-        t0 ^= rk[4*round + 0];
-        t1 ^= rk[4*round + 1];
-        t2 ^= rk[4*round + 2];
-        t3 ^= rk[4*round + 3];
-
-        //
-        // Inv MixColumns
-
-        t0 = _saes_v1_decm(t0);
-        t1 = _saes_v1_decm(t1);
-        t2 = _saes_v1_decm(t2);
-        t3 = _saes_v1_decm(t3);
-
-    }
-
-    //
-    // Inv ShiftRows
-    
-    n0 = (t0 & 0x000000FF) | (t3 & 0x0000FF00) |
-         (t2 & 0x00FF0000) | (t1 & 0xFF000000) ;
-    
-    n1 = (t1 & 0x000000FF) | (t0 & 0x0000FF00) | 
-         (t3 & 0x00FF0000) | (t2 & 0xFF000000) ;
-    
-    n2 = (t2 & 0x000000FF) | (t1 & 0x0000FF00) |
-         (t0 & 0x00FF0000) | (t3 & 0xFF000000) ;
-
-    n3 = (t3 & 0x000000FF) | (t2 & 0x0000FF00) |
-         (t1 & 0x00FF0000) | (t0 & 0xFF000000) ;
-
-    //
-    // Inv SubBytes
-
-    t0 = _saes_v1_decs(n0);
-    t1 = _saes_v1_decs(n1);
-    t2 = _saes_v1_decs(n2);
-    t3 = _saes_v1_decs(n3);
-
-    //
-    // Add Round Key
-
-    t0 ^= rk[4*round + 0];
-    t1 ^= rk[4*round + 1];
-    t2 ^= rk[4*round + 2];
-    t3 ^= rk[4*round + 3];
-    
-    U32_TO_U8_LE(pt, t0, 0);
-    U32_TO_U8_LE(pt, t1, 4);
-    U32_TO_U8_LE(pt, t2, 8);
-    U32_TO_U8_LE(pt, t3,12);
-}
+);
 
 void    aes_128_dec_key_schedule (
     uint32_t    rk [AES_128_RK_WORDS],
